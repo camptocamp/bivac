e6a252902276ec714c236e33347aa4d1eda9e059
Mickaël Canévet <mickael.canevet@camptocamp.com>
2016-04-01 16:49:59 +0200

    Set Cmd and Env

--- docker-backup.go
+++ docker-backup.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "fmt"
+  "os"
   "github.com/fsouza/go-dockerclient"
 )
 
@@ -12,6 +13,7 @@ func main() {
   client, _ := docker.NewClient(endpoint)
   vols, _ := client.ListVolumes(docker.ListVolumesOptions{})
   for _, vol := range vols {
+    // TODO: filter out unnamed volumes (name has 64 characters?)
     fmt.Println("ID: ", vol.Name)
     fmt.Println("Driver: ", vol.Driver)
     fmt.Println("Mountpoint: ", vol.Mountpoint)
@@ -19,7 +21,26 @@ func main() {
     client.CreateContainer(
       docker.CreateContainerOptions{
         Config: &docker.Config{
-         Image: "camptocamp/duplicity",
+          Cmd: []string{
+            "--full-if-older-than", "15D",
+            "--s3-use-new-style",
+            "--no-encryption",
+            "--allow-source-mismatch",
+            "/var/backups",
+            os.Getenv("DUPLICITY_TARGET_URL"),
+          },
+          Env: []string{
+            "AWS_ACCESS_KEY_ID="+os.Getenv("AWS_ACCESS_KEY_ID"),
+            "AWS_SECRET_ACCESS_KEY="+os.Getenv("AWS_SECRET_ACCESS_KEY"),
+            "SWIFT_USERNAME="+os.Getenv("SWIFT_USERNAME"),
+            "SWIFT_PASSWORD="+os.Getenv("SWIFT_PASSWORD"),
+            "SWIFT_AUTHURL="+os.Getenv("SWIFT_AUTHURL"),
+            "SWIFT_TENANTNAME="+os.Getenv("SWIFT_TENANTNAME"),
+            "SWIFT_REGIONNAME="+os.Getenv("SWIFT_REGIONNAME"),
+            "SWIFT_AUTHVERSION=2",
+          },
+          Image: "camptocamp/duplicity",
+          // TODO: mount volume in /var/backups
         },
       },
     )
