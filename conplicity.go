ed775dcfe5b10b45ca5831dbe4f0ea2ad6632552
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-04-04 15:24:40 +0200

    Refactor: funcs and use logrus

--- conplicity.go
+++ conplicity.go
@@ -1,33 +1,56 @@
 package main
 
 import (
-  "fmt"
   "os"
   "unicode/utf8"
   "github.com/fsouza/go-dockerclient"
   "github.com/fgrehm/go-dockerpty"
+	log "github.com/Sirupsen/logrus"
 )
 
-func main() {
-  fmt.Println("Starting backup...")
+type Conplicity struct {
+  Hostname string
+  Client   *docker.Client
+}
 
-  hostname, _ := os.Hostname()
+func main() {
+  log.Infof("Starting backup...")
+
+  var err error
+
+  c := &Conplicity{}
+  c.Hostname, err = os.Hostname()
+  checkErr(err, "Failed to get hostname: %v", 1)
 
   endpoint := "unix:///var/run/docker.sock"
-  client, _ := docker.NewClient(endpoint)
-  vols, _ := client.ListVolumes(docker.ListVolumesOptions{})
+
+  c.Client, err = docker.NewClient(endpoint)
+  checkErr(err, "Failed to create Docker client: %v", 1)
+
+  vols, err := c.Client.ListVolumes(docker.ListVolumesOptions{})
+  checkErr(err, "Failed to list Docker volumes: %v", 1)
+
   for _, vol := range vols {
+    err = c.backupVolume(vol)
+    checkErr(err, "Failed to process volume "+vol.Name+": %v", -1)
+  }
+
+  log.Infof("End backup...")
+}
+
+
+func (c *Conplicity) backupVolume(vol docker.Volume) (err error) {
     if utf8.RuneCountInString(vol.Name) == 64 {
-      fmt.Println("Ignoring volume ", vol.Name)
-      continue
+      log.Infof("Ignoring volume ", vol.Name)
+      return
     }
 
     // TODO: detect if it's a Database volume (PostgreSQL, MySQL, OpenLDAP...) and launch DUPLICITY_PRECOMMAND instead of backuping the volume
-    fmt.Println("ID: ", vol.Name)
-    fmt.Println("Driver: ", vol.Driver)
-    fmt.Println("Mountpoint: ", vol.Mountpoint)
-    fmt.Println("Creating duplicity container...")
-    container, err := client.CreateContainer(
+    log.Infof("ID: ", vol.Name)
+    log.Infof("Driver: ", vol.Driver)
+    log.Infof("Mountpoint: ", vol.Mountpoint)
+    log.Infof("Creating duplicity container...")
+    container, err := c.Client.CreateContainer(
       docker.CreateContainerOptions{
         Config: &docker.Config{
           Cmd: []string{
@@ -36,7 +59,7 @@ func main() {
             "--no-encryption",
             "--allow-source-mismatch",
             "/var/backups",
-            os.Getenv("DUPLICITY_TARGET_URL")+"/"+hostname+"/"+vol.Name,
+            os.Getenv("DUPLICITY_TARGET_URL")+"/"+c.Hostname+"/"+vol.Name,
           },
           Env: []string{
             "AWS_ACCESS_KEY_ID="+os.Getenv("AWS_ACCESS_KEY_ID"),
@@ -59,13 +82,10 @@ func main() {
       },
     )
 
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
-    }
+    checkErr(err, "Failed to create container for volume "+vol.Name+": %v", 1)
 
     defer func() {
-      client.RemoveContainer(docker.RemoveContainerOptions{
+      c.Client.RemoveContainer(docker.RemoveContainerOptions{
         ID: container.ID,
         Force: true,
       })
@@ -75,16 +95,19 @@ func main() {
       vol.Mountpoint+":/var/backups:ro",
     }
 
-    fmt.Println("Starting duplicity container...")
-    err = dockerpty.Start(client, container, &docker.HostConfig{
+    err = dockerpty.Start(c.Client, container, &docker.HostConfig{
       Binds: binds,
     })
+    checkErr(err, "Failed to start container for volume "+vol.Name+": %v", -1)
+    return
+}
 
-    if err != nil {
-      fmt.Println(err)
-      os.Exit(1)
+func checkErr(err error, msg string, exit int) {
+  if err != nil {
+    log.Errorf(msg, err)
+
+    if exit != -1 {
+      os.Exit(exit)
     }
   }
-
-  fmt.Println("End backup...")
 }
