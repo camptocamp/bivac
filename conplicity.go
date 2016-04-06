b566eddc5a5ba17b770593f8c29e32eade311fd3
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-04-06 09:29:14 +0200

    Lint with goimports

--- conplicity.go
+++ conplicity.go
@@ -1,161 +1,162 @@
 package main
 
 import (
-  "os"
-  "unicode/utf8"
-  "github.com/fsouza/go-dockerclient"
-  "github.com/fgrehm/go-dockerpty"
+	"os"
+	"unicode/utf8"
+
 	log "github.com/Sirupsen/logrus"
+	"github.com/fgrehm/go-dockerpty"
+	"github.com/fsouza/go-dockerclient"
 )
 
 type environment struct {
-  Image              string
-  DuplicityTargetURL string
-  AWSAccessKeyID     string
-  AWSSecretAccessKey string
-  SwiftUsername      string
-  SwiftPassword      string
-  SwiftAuthURL       string
-  SwiftTenantName    string
-  SwiftRegionName    string
+	Image              string
+	DuplicityTargetURL string
+	AWSAccessKeyID     string
+	AWSSecretAccessKey string
+	SwiftUsername      string
+	SwiftPassword      string
+	SwiftAuthURL       string
+	SwiftTenantName    string
+	SwiftRegionName    string
 }
 
 type conplicity struct {
-           *docker.Client
-           *environment
-  Hostname string
+	*docker.Client
+	*environment
+	Hostname string
 }
 
 func main() {
-  log.Infof("Starting backup...")
+	log.Infof("Starting backup...")
 
-  var err error
+	var err error
 
-  c := &conplicity{}
+	c := &conplicity{}
 
-  c.getEnv()
+	c.getEnv()
 
-  c.Hostname, err = os.Hostname()
-  checkErr(err, "Failed to get hostname: %v", 1)
+	c.Hostname, err = os.Hostname()
+	checkErr(err, "Failed to get hostname: %v", 1)
 
-  endpoint := "unix:///var/run/docker.sock"
+	endpoint := "unix:///var/run/docker.sock"
 
-  c.Client, err = docker.NewClient(endpoint)
-  checkErr(err, "Failed to create Docker client: %v", 1)
+	c.Client, err = docker.NewClient(endpoint)
+	checkErr(err, "Failed to create Docker client: %v", 1)
 
-  vols, err := c.ListVolumes(docker.ListVolumesOptions{})
-  checkErr(err, "Failed to list Docker volumes: %v", 1)
+	vols, err := c.ListVolumes(docker.ListVolumesOptions{})
+	checkErr(err, "Failed to list Docker volumes: %v", 1)
 
-  err = c.pullImage()
-  checkErr(err, "Failed to pull image: %v", 1)
+	err = c.pullImage()
+	checkErr(err, "Failed to pull image: %v", 1)
 
-  for _, vol := range vols {
-    err = c.backupVolume(vol)
-    checkErr(err, "Failed to process volume "+vol.Name+": %v", -1)
-  }
+	for _, vol := range vols {
+		err = c.backupVolume(vol)
+		checkErr(err, "Failed to process volume "+vol.Name+": %v", -1)
+	}
 
-  log.Infof("End backup...")
+	log.Infof("End backup...")
 }
 
 func (c *conplicity) getEnv() (err error) {
-  c.environment = &environment{
-    Image: os.Getenv("DUPLICITY_DOCKER_IMAGE"),
-    DuplicityTargetURL: os.Getenv("DUPLICITY_TARGET_URL"),
-    AWSAccessKeyID: os.Getenv("AWS_ACCESS_KEY_ID"),
-    AWSSecretAccessKey: os.Getenv("AWS_SECRET_ACCESS_KEY"),
-    SwiftUsername: os.Getenv("SWIFT_USERNAME"),
-    SwiftPassword: os.Getenv("SWIFT_PASSWORD"),
-    SwiftAuthURL: os.Getenv("SWIFT_AUTHURL"),
-    SwiftTenantName: os.Getenv("SWIFT_TENANTNAME"),
-    SwiftRegionName: os.Getenv("SWIFT_REGIONNAME"),
-  }
+	c.environment = &environment{
+		Image:              os.Getenv("DUPLICITY_DOCKER_IMAGE"),
+		DuplicityTargetURL: os.Getenv("DUPLICITY_TARGET_URL"),
+		AWSAccessKeyID:     os.Getenv("AWS_ACCESS_KEY_ID"),
+		AWSSecretAccessKey: os.Getenv("AWS_SECRET_ACCESS_KEY"),
+		SwiftUsername:      os.Getenv("SWIFT_USERNAME"),
+		SwiftPassword:      os.Getenv("SWIFT_PASSWORD"),
+		SwiftAuthURL:       os.Getenv("SWIFT_AUTHURL"),
+		SwiftTenantName:    os.Getenv("SWIFT_TENANTNAME"),
+		SwiftRegionName:    os.Getenv("SWIFT_REGIONNAME"),
+	}
 
-  if c.Image == "" {
-    c.Image = "camptocamp/duplicity:latest"
-  }
+	if c.Image == "" {
+		c.Image = "camptocamp/duplicity:latest"
+	}
 
-  return
+	return
 }
 
 func (c *conplicity) backupVolume(vol docker.Volume) (err error) {
-    if utf8.RuneCountInString(vol.Name) == 64 {
-      log.Infof("Ignoring volume "+vol.Name)
-      return
-    }
+	if utf8.RuneCountInString(vol.Name) == 64 {
+		log.Infof("Ignoring volume " + vol.Name)
+		return
+	}
 
-    // TODO: detect if it's a Database volume (PostgreSQL, MySQL, OpenLDAP...) and launch DUPLICITY_PRECOMMAND instead of backuping the volume
-    log.Infof("ID: "+vol.Name)
-    log.Infof("Driver: "+vol.Driver)
-    log.Infof("Mountpoint: "+vol.Mountpoint)
-    log.Infof("Creating duplicity container...")
-    container, err := c.CreateContainer(
-      docker.CreateContainerOptions{
-        Config: &docker.Config{
-          Cmd: []string{
-            "--full-if-older-than", "15D",
-            "--s3-use-new-style",
-            "--no-encryption",
-            "--allow-source-mismatch",
-            "/var/backups",
-            c.DuplicityTargetURL+"/"+c.Hostname+"/"+vol.Name,
-          },
-          Env: []string{
-            "AWS_ACCESS_KEY_ID="+c.AWSAccessKeyID,
-            "AWS_SECRET_ACCESS_KEY="+c.AWSSecretAccessKey,
-            "SWIFT_USERNAME="+c.SwiftUsername,
-            "SWIFT_PASSWORD="+c.SwiftPassword,
-            "SWIFT_AUTHURL="+c.SwiftAuthURL,
-            "SWIFT_TENANTNAME="+c.SwiftTenantName,
-            "SWIFT_REGIONNAME="+c.SwiftRegionName,
-            "SWIFT_AUTHVERSION=2",
-          },
-          Image: c.Image,
-          OpenStdin:    true,
-          StdinOnce:    true,
-          AttachStdin:  true,
-          AttachStdout: true,
-          AttachStderr: true,
-          Tty:          true,
-        },
-      },
-    )
+	// TODO: detect if it's a Database volume (PostgreSQL, MySQL, OpenLDAP...) and launch DUPLICITY_PRECOMMAND instead of backuping the volume
+	log.Infof("ID: " + vol.Name)
+	log.Infof("Driver: " + vol.Driver)
+	log.Infof("Mountpoint: " + vol.Mountpoint)
+	log.Infof("Creating duplicity container...")
+	container, err := c.CreateContainer(
+		docker.CreateContainerOptions{
+			Config: &docker.Config{
+				Cmd: []string{
+					"--full-if-older-than", "15D",
+					"--s3-use-new-style",
+					"--no-encryption",
+					"--allow-source-mismatch",
+					"/var/backups",
+					c.DuplicityTargetURL + "/" + c.Hostname + "/" + vol.Name,
+				},
+				Env: []string{
+					"AWS_ACCESS_KEY_ID=" + c.AWSAccessKeyID,
+					"AWS_SECRET_ACCESS_KEY=" + c.AWSSecretAccessKey,
+					"SWIFT_USERNAME=" + c.SwiftUsername,
+					"SWIFT_PASSWORD=" + c.SwiftPassword,
+					"SWIFT_AUTHURL=" + c.SwiftAuthURL,
+					"SWIFT_TENANTNAME=" + c.SwiftTenantName,
+					"SWIFT_REGIONNAME=" + c.SwiftRegionName,
+					"SWIFT_AUTHVERSION=2",
+				},
+				Image:        c.Image,
+				OpenStdin:    true,
+				StdinOnce:    true,
+				AttachStdin:  true,
+				AttachStdout: true,
+				AttachStderr: true,
+				Tty:          true,
+			},
+		},
+	)
 
-    checkErr(err, "Failed to create container for volume "+vol.Name+": %v", 1)
+	checkErr(err, "Failed to create container for volume "+vol.Name+": %v", 1)
 
-    defer func() {
-      c.RemoveContainer(docker.RemoveContainerOptions{
-        ID: container.ID,
-        Force: true,
-      })
-    }()
+	defer func() {
+		c.RemoveContainer(docker.RemoveContainerOptions{
+			ID:    container.ID,
+			Force: true,
+		})
+	}()
 
-    binds := []string{
-      vol.Mountpoint+":/var/backups:ro",
-    }
+	binds := []string{
+		vol.Mountpoint + ":/var/backups:ro",
+	}
 
-    err = dockerpty.Start(c.Client, container, &docker.HostConfig{
-      Binds: binds,
-    })
-    checkErr(err, "Failed to start container for volume "+vol.Name+": %v", -1)
-    return
+	err = dockerpty.Start(c.Client, container, &docker.HostConfig{
+		Binds: binds,
+	})
+	checkErr(err, "Failed to start container for volume "+vol.Name+": %v", -1)
+	return
 }
 
 func (c *conplicity) pullImage() (err error) {
-  // TODO: output pull to logs
-  log.Infof("Pulling image %v", c.Image)
-  err = c.PullImage(docker.PullImageOptions{
-    Repository: c.Image,
-  }, docker.AuthConfiguration{})
+	// TODO: output pull to logs
+	log.Infof("Pulling image %v", c.Image)
+	err = c.PullImage(docker.PullImageOptions{
+		Repository: c.Image,
+	}, docker.AuthConfiguration{})
 
-  return err
+	return err
 }
 
 func checkErr(err error, msg string, exit int) {
-  if err != nil {
-    log.Errorf(msg, err)
+	if err != nil {
+		log.Errorf(msg, err)
 
-    if exit != -1 {
-      os.Exit(exit)
-    }
-  }
+		if exit != -1 {
+			os.Exit(exit)
+		}
+	}
 }
