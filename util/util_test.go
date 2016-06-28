fd4aa8aaaf139a2b0e69f06177d56f6d8093c6c1
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-06-28 11:05:38 +0200

    test CheckErr

--- util/util_test.go
+++ util/util_test.go
@@ -1,21 +1,40 @@
 package util
 
 import (
+	"errors"
 	"testing"
 
 	"github.com/fsouza/go-dockerclient"
 )
 
-var vol = docker.Volume{
+var fakeVol = docker.Volume{
 	Name: "Test",
 	Labels: map[string]string{
 		"io.conplicity.test": "Fake label",
 	},
 }
 
+func ExampleCheckErrNil() {
+	CheckErr(nil, "test", 0)
+	// Output:
+}
+
+func ExampleCheckErrNoExit() {
+	fakeErr := errors.New("Fake error %v")
+	CheckErr(fakeErr, "test", 0)
+	// Output:
+}
+
+func ExampleCheckErrExit() {
+	fakeErr := errors.New("Fake error %v")
+	CheckErr(fakeErr, "test", -1)
+	// Output:
+	// How do we test the exit?
+}
+
 func TestVolumeLabel(t *testing.T) {
 	expectedStr := "Fake label"
-	result, _ := GetVolumeLabel(&vol, ".test")
+	result, _ := GetVolumeLabel(&fakeVol, ".test")
 	if result != expectedStr {
 		t.Fatalf("Expected %s, got %s", expectedStr, result)
 	}
@@ -24,7 +43,7 @@ func TestVolumeLabel(t *testing.T) {
 func TestVolumeLabelNotFound(t *testing.T) {
 	expectedStr := ""
 	expectedErr := "Key .unknown not found in labels for volume Test"
-	result, err := GetVolumeLabel(&vol, ".unknown")
+	result, err := GetVolumeLabel(&fakeVol, ".unknown")
 	if result != expectedStr {
 		t.Fatalf("Expected %s, got %s", expectedStr, result)
 	}
