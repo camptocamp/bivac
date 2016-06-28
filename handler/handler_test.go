5a9c4b759892f809171952a1d0bb7fa4b6451069
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-06-28 11:39:33 +0200

    Test loglevel setting

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -1,6 +1,10 @@
 package handler
 
-import "testing"
+import (
+	"testing"
+
+	log "github.com/Sirupsen/logrus"
+)
 
 var fakeHandler = Conplicity{}
 
@@ -17,5 +21,24 @@ func TestSetup(t *testing.T) {
 	gotEndpoint := fakeHandler.Client.Endpoint()
 	if gotEndpoint != expectedEndpoint {
 		t.Fatalf("Expected %s, got %s", expectedEndpoint, gotEndpoint)
+	}
+
+	// Check default Loglevel
+	if l := log.GetLevel(); l != log.InfoLevel {
+		t.Fatalf("Expected %v loglevel by default, got %v", log.InfoLevel, l)
+	}
+
+	// Check setting Loglevel
+	fakeHandler.Config.Loglevel = "debug"
+	fakeHandler.setupLoglevel()
+	if l := log.GetLevel(); l != log.DebugLevel {
+		t.Fatalf("Expected %v loglevel, got %v", log.DebugLevel, l)
+	}
+
+	// Check setting Loglevel to wrong value
+	fakeHandler.Config.Loglevel = "wrong"
+	err := fakeHandler.setupLoglevel()
+	if err == nil {
+		t.Fatal("Expected setupLoglevel to fail")
 	}
 }
