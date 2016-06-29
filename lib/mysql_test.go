bcd2fc89a7aa4c5ae45b2f1eba894dff4a214407
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-06-29 11:02:59 +0200

    Add mysql_test

--- lib/mysql_test.go
+++ lib/mysql_test.go
@@ -6,23 +6,23 @@ import (
 	"github.com/docker/engine-api/types"
 )
 
-func TestPostgreSQLGetName(t *testing.T) {
-	expected := "PostgreSQL"
-	got := (&PostgreSQLProvider{}).GetName()
+func TestMySQLGetName(t *testing.T) {
+	expected := "MySQL"
+	got := (&MySQLProvider{}).GetName()
 	if expected != got {
 		t.Fatalf("Expected %s, got %s", expected, got)
 	}
 }
 
-func TestPostgreSQLGetBackupDir(t *testing.T) {
+func TestMySQLGetBackupDir(t *testing.T) {
 	expected := "backups"
-	got := (&PostgreSQLProvider{}).GetBackupDir()
+	got := (&MySQLProvider{}).GetBackupDir()
 	if expected != got {
 		t.Fatalf("Expected %s, got %s", expected, got)
 	}
 }
 
-func TestPostgreSQLGetPrepareCommand(t *testing.T) {
+func TestMySQLGetPrepareCommand(t *testing.T) {
 	mount := &types.MountPoint{
 		Destination: "/mnt",
 	}
@@ -30,9 +30,9 @@ func TestPostgreSQLGetPrepareCommand(t *testing.T) {
 	expected := []string{
 		"sh",
 		"-c",
-		"mkdir -p /mnt/backups && pg_dumpall -Upostgres > /mnt/backups/all.sql",
+		"mkdir -p /mnt/backups && mysqldump --all-databases --extended-insert --password=$MYSQL_ROOT_PASSWORD > /mnt/backups/all.sql",
 	}
-	got := (&PostgreSQLProvider{}).GetPrepareCommand(mount)
+	got := (&MySQLProvider{}).GetPrepareCommand(mount)
 	if len(got) != 3 {
 		t.Fatalf("Expected command to have 3 elements, got %s", len(got))
 	} else {
