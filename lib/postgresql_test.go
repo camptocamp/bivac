dd331cfe2b54c08aff83f0ee367d7058218ae6c8
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-06-29 11:00:39 +0200

    Add postgresql_test

---
+++ lib/postgresql_test.go
@@ -0,0 +1,43 @@
+package conplicity
+
+import (
+	"testing"
+
+	"github.com/docker/engine-api/types"
+)
+
+func TestGetName(t *testing.T) {
+	expected := "PostgreSQL"
+	got := (&PostgreSQLProvider{}).GetName()
+	if expected != got {
+		t.Fatalf("Expected %s, got %s", expected, got)
+	}
+}
+
+func TestGetBackupDir(t *testing.T) {
+	expected := "backups"
+	got := (&PostgreSQLProvider{}).GetBackupDir()
+	if expected != got {
+		t.Fatalf("Expected %s, got %s", expected, got)
+	}
+}
+
+func TestGetPrepareCommand(t *testing.T) {
+	mount := &types.MountPoint{
+		Destination: "/mnt",
+	}
+
+	expected := []string{
+		"sh",
+		"-c",
+		"mkdir -p /mnt/backups && pg_dumpall -Upostgres > /mnt/backups/all.sql",
+	}
+	got := (&PostgreSQLProvider{}).GetPrepareCommand(mount)
+	if len(got) != 3 {
+		t.Fatalf("Expected command to have 3 elements, got %s", len(got))
+	} else {
+		if expected[2] != got[2] {
+			t.Fatalf("Expected %s, got %s", expected, got)
+		}
+	}
+}
