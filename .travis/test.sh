7cf5572fa94fdcfd21428eef60ea3e26fba97afa
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-06-30 11:07:46 +0200

    Test docker image

---
+++ .travis/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+docker build -t testtag .
+
+docker run -v /var/run/docker.sock:/var/run/docker.sock:ro  --rm -ti \
+  -e DUPLICITY_TARGET_URL="file:///root/.cache/duplicity/.test_backup" \
+  testtag -l debug
+
+if [ "$?" -ne 0 ]; then
+  echo "E: Image test failed"
+  exit 1
+fi
