3df3dbda1ab9909a0a6f00733fb943237144e5f2
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-07-28 15:21:16 +0200

    Remove legagy Handler interface

--- volume/volume.go
+++ volume/volume.go
@@ -2,11 +2,6 @@ package volume
 
 import "github.com/docker/engine-api/types"
 
-// Handler is an interface providing a mean of launching Duplicity
-type Handler interface {
-	LaunchDuplicity([]string, []string) (int, string, error)
-}
-
 // Volume provides backup methods for a single Docker volume
 type Volume struct {
 	*types.Volume
