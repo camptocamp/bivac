90d8256b1e4ca58e88f2b824e7e432e02b725fe5
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-07-28 12:55:54 +0200

    Put config aside

--- lib/handler.go
+++ lib/handler.go
@@ -7,7 +7,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os"
-	"sort"
 	"strings"
 
 	"golang.org/x/net/context"
@@ -16,47 +15,7 @@ import (
 	docker "github.com/docker/engine-api/client"
 	"github.com/docker/engine-api/types"
 	"github.com/docker/engine-api/types/container"
-	"github.com/jessevdk/go-flags"
 )
-
-// Config stores the handler's configuration and UI interface parameters
-type Config struct {
-	Version          bool     `short:"V" long:"version" description:"Display version."`
-	Loglevel         string   `short:"l" long:"loglevel" description:"Set loglevel ('debug', 'info', 'warn', 'error', 'fatal', 'panic')." env:"CONPLICITY_LOG_LEVEL" default:"info"`
-	VolumesBlacklist []string `short:"b" long:"blacklist" description:"Volumes to blacklist in backups." env:"CONPLICITY_VOLUMES_BLACKLIST" env-delim:","`
-	Manpage          bool     `short:"m" long:"manpage" description:"Output manpage."`
-	NoVerify         bool     `long:"no-verify" description:"Do not verify backup." env:"CONPLICITY_NO_VERIFY"`
-	JSON             bool     `short:"j" long:"json" description:"Log as JSON (to stderr)." env:"CONPLICITY_JSON_OUTPUT"`
-	Engine           string   `short:"E" long:"engine" description:"Backup engine to use." env:"CONPLICITY_ENGINE" default:"duplicity"`
-
-	Duplicity struct {
-		Image           string `short:"i" long:"duplicity-image" description:"The duplicity docker image." env:"DUPLICITY_DOCKER_IMAGE" default:"camptocamp/duplicity:latest"`
-		TargetURL       string `short:"u" long:"url" description:"The duplicity target URL to push to." env:"DUPLICITY_TARGET_URL"`
-		FullIfOlderThan string `long:"full-if-older-than" description:"The number of days after which a full backup must be performed." env:"CONPLICITY_FULL_IF_OLDER_THAN" default:"15D"`
-		RemoveOlderThan string `long:"remove-older-than" description:"The number days after which backups must be removed." env:"CONPLICITY_REMOVE_OLDER_THAN" default:"30D"`
-	} `group:"Duplicity Options"`
-
-	Metrics struct {
-		PushgatewayURL string `short:"g" long:"gateway-url" description:"The prometheus push gateway URL to use." env:"PUSHGATEWAY_URL"`
-	} `group:"Metrics Options"`
-
-	AWS struct {
-		AccessKeyID     string `long:"aws-access-key-id" description:"The AWS access key ID." env:"AWS_ACCESS_KEY_ID"`
-		SecretAccessKey string `long:"aws-secret-key-id" description:"The AWS secret access key." env:"AWS_SECRET_ACCESS_KEY"`
-	} `group:"AWS Options"`
-
-	Swift struct {
-		Username   string `long:"swift-username" description:"The Swift user name." env:"SWIFT_USERNAME"`
-		Password   string `long:"swift-password" description:"The Swift password." env:"SWIFT_PASSWORD"`
-		AuthURL    string `long:"swift-auth_url" description:"The Swift auth URL." env:"SWIFT_AUTHURL"`
-		TenantName string `long:"swift-tenant-name" description:"The Swift tenant name." env:"SWIFT_TENANTNAME"`
-		RegionName string `long:"swift-region-name" description:"The Swift region name." env:"SWIFT_REGIONNAME"`
-	} `group:"Swift Options"`
-
-	Docker struct {
-		Endpoint string `short:"e" long:"docker-endpoint" description:"The Docker endpoint." env:"DOCKER_ENDPOINT" default:"unix:///var/run/docker.sock"`
-	} `group:"Docker Options"`
-}
 
 // Conplicity is the main handler struct
 type Conplicity struct {
@@ -68,7 +27,7 @@ type Conplicity struct {
 
 // Setup sets up a Conplicity struct
 func (c *Conplicity) Setup(version string) (err error) {
-	c.getEnv(version)
+	c.Config = LoadConfig(version)
 
 	err = c.setupLoglevel()
 	CheckErr(err, "Failed to setup log level: %v", "fatal")
@@ -86,29 +45,6 @@ func (c *Conplicity) Setup(version string) (err error) {
 func (c *Conplicity) SetupDocker() (err error) {
 	c.Client, err = docker.NewClient(c.Config.Docker.Endpoint, "", nil, nil)
 	CheckErr(err, "Failed to create Docker client: %v", "fatal")
-	return
-}
-
-func (c *Conplicity) getEnv(version string) (err error) {
-	c.Config = &Config{}
-	parser := flags.NewParser(c.Config, flags.Default)
-	if _, err = parser.Parse(); err != nil {
-		os.Exit(1)
-	}
-
-	if c.Config.Version {
-		fmt.Printf("Conplicity v%v\n", version)
-		os.Exit(0)
-	}
-
-	if c.Config.Manpage {
-		var buf bytes.Buffer
-		parser.WriteManPage(&buf)
-		fmt.Printf(buf.String())
-		os.Exit(0)
-	}
-
-	sort.Strings(c.Config.VolumesBlacklist)
 	return
 }
 
