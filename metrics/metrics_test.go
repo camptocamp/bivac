c7e317539b9482566e484ebd0b19a21849734381
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-10 14:00:19 +0200

    TestNewMetric

--- metrics/metrics_test.go
+++ metrics/metrics_test.go
@@ -62,3 +62,24 @@ func TestEventString(t *testing.T) {
 		t.Fatal("Expected %s, got %s", expected, e.String())
 	}
 }
+
+func TestNewMetric(t *testing.T) {
+	p := NewMetrics("foo", "http://foo:9091")
+	m := p.NewMetric("bar", "qux")
+
+	if len(p.Metrics) != 1 {
+		t.Fatalf("Expected 1 metric, got %i", len(p.Metrics))
+	}
+
+	if p.Metrics["bar"] != m {
+		t.Fatal("Expected to find metric in handler")
+	}
+
+	if m.Name != "bar" {
+		t.Fatalf("Expected name to be bar, got %s", m.Name)
+	}
+
+	if m.Type != "qux" {
+		t.Fatalf("Expected type to be qux, got %s", m.Name)
+	}
+}
