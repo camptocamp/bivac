cbbdc18fd3b09eefc36927987a1d8fe9a9daba54
Raphaël Pinson <raphael.pinson@camptocamp.com>
2016-08-17 12:39:44 +0200

    Fix example names

--- util/util_test.go
+++ util/util_test.go
@@ -14,18 +14,18 @@ var fakeDockerVol = types.Volume{
 	},
 }
 
-func ExampleCheckErrNil() {
+func ExampleCheckErr_nil() {
 	CheckErr(nil, "test", "fatal")
 	// Output:
 }
 
-func ExampleCheckErrNoExit() {
+func ExampleCheckErr_noExit() {
 	fakeErr := errors.New("Fake error")
 	CheckErr(fakeErr, "test: %v", "error")
 	// Output:
 }
 
-func ExampleCheckErrExit() {
+func ExampleCheckErr_exit() {
 	// fakeErr := errors.New("Fake error")
 	// How do we test the output and the os.Exit(1)?"
 	// CheckErr(fakeErr, "test: %v", "fatal")
