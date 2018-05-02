9f0c6b1e18f3ebe3304349d944675648e965a3fb
Raphaël Pinson <raphael.pinson@camptocamp.com>
2018-05-02 21:08:57 +0200

    Make Restic the default engine

--- config/config.go
+++ config/config.go
@@ -17,7 +17,7 @@ type Config struct {
 	Manpage             bool     `short:"m" long:"manpage" description:"Output manpage."`
 	NoVerify            bool     `long:"no-verify" description:"Do not verify backup." env:"BIVAC_NO_VERIFY"`
 	JSON                bool     `short:"j" long:"json" description:"Log as JSON (to stderr)." env:"BIVAC_JSON_OUTPUT"`
-	Engine              string   `short:"E" long:"engine" description:"Backup engine to use." env:"BIVAC_ENGINE" default:"duplicity"`
+	Engine              string   `short:"E" long:"engine" description:"Backup engine to use." env:"BIVAC_ENGINE" default:"restic"`
 	Orchestrator        string   `short:"o" long:"orchestrator" description:"Container orchestrator to use." env:"BIVAC_ORCHESTRATOR" default:"docker"`
 	TargetURL           string   `short:"u" long:"target-url" description:"The target URL to push to." env:"BIVAC_TARGET_URL"`
 	HostnameFromRancher bool     `short:"H" long:"hostname-from-rancher" description:"Retrieve hostname from Rancher metadata." env:"BIVAC_HOSTNAME_FROM_RANCHER"`
@@ -25,19 +25,19 @@ type Config struct {
 	RemoveOlderThan     string   `long:"remove-older-than" description:"Remove backups older than the specified interval." env:"BIVAC_REMOVE_OLDER_THAN" default:"30D"`
 	LabelPrefix         string   `long:"label-prefix" description:"The volume prefix label." env:"BIVAC_LABEL_PREFIX"`
 
-	Duplicity struct {
-		Image           string `long:"duplicity-image" description:"The duplicity docker image." env:"DUPLICITY_DOCKER_IMAGE" default:"camptocamp/duplicity:latest"`
-		FullIfOlderThan string `long:"full-if-older-than" description:"The number of days after which a full backup must be performed." env:"BIVAC_FULL_IF_OLDER_THAN" default:"15D"`
-	} `group:"Duplicity Options"`
+	Restic struct {
+		Image    string `long:"restic-image" description:"The restic docker image." env:"RESTIC_DOCKER_IMAGE" default:"restic/restic:latest"`
+		Password string `long:"restic-password" description:"The restic backup password." env:"RESTIC_PASSWORD"`
+	} `group:"Restic Options"`
 
 	RClone struct {
 		Image string `long:"rclone-image" description:"The rclone docker image." env:"RCLONE_DOCKER_IMAGE" default:"camptocamp/rclone:1.33-1"`
 	} `group:"RClone Options"`
 
-	Restic struct {
-		Image    string `long:"restic-image" description:"The restic docker image." env:"RESTIC_DOCKER_IMAGE" default:"restic/restic:latest"`
-		Password string `long:"restic-password" description:"The restic backup password." env:"RESTIC_PASSWORD"`
-	} `group:"Restic Options"`
+	Duplicity struct {
+		Image           string `long:"duplicity-image" description:"The duplicity docker image." env:"DUPLICITY_DOCKER_IMAGE" default:"camptocamp/duplicity:latest"`
+		FullIfOlderThan string `long:"full-if-older-than" description:"The number of days after which a full backup must be performed." env:"BIVAC_FULL_IF_OLDER_THAN" default:"15D"`
+	} `group:"Duplicity Options"`
 
 	Metrics struct {
 		PushgatewayURL string `short:"g" long:"gateway-url" description:"The prometheus push gateway URL to use." env:"PUSHGATEWAY_URL"`
@@ -86,11 +86,11 @@ func LoadConfig(version string) *Config {
 
 Bivac supports multiple engines for performing the backup:
 
-* Duplicity (default engine)
+* Restic (default engine)
 
-* RClone: use for heavy data that Duplicity cannot manage efficiently
+* RClone: use for heavy data that Restic/Duplicity cannot manage efficiently
 
-* Restic
+* Duplicity
 `
 		parser.WriteManPage(&buf)
 		fmt.Printf(buf.String())
