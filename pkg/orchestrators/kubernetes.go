9e3c802f91e20623c1e2fe8e4f217039f4299c6e
Jam Risser <jam@codejam.ninja>
2019-05-03 04:48:06 -0500

    Added kubernetes whitelist annotation support (#294)

--- pkg/orchestrators/kubernetes.go
+++ pkg/orchestrators/kubernetes.go
@@ -74,6 +74,17 @@ func (o *KubernetesOrchestrator) GetVolumes(volumeFilters volume.Filters) (volum
 		}
 
 		for _, pvc := range pvcs.Items {
+			if backupString, ok := pvc.Annotations["bivac.backup"]; ok {
+				if volumeFilters.WhitelistAnnotation {
+					if strings.ToLower(backupString) != "true" {
+						continue
+					}
+				} else {
+					if strings.ToLower(backupString) == "false" {
+						continue
+					}
+				}
+			}
 			v := &volume.Volume{
 				ID:        string(pvc.UID),
 				Name:      pvc.Name,
