5530855e49436240c365cc5df57020863d527e01
Jam Risser <jam@codejam.ninja>
2019-05-30 20:52:54 -0500

    Revert "Removed vendor"
    This reverts commit f7c1ad8ce754471befbbadfa7de6e51f24d902c4.

---
+++ vendor/github.com/rancher/go-rancher/v2/generated_port_rule.go
@@ -0,0 +1,101 @@
+package client
+
+const (
+	PORT_RULE_TYPE = "portRule"
+)
+
+type PortRule struct {
+	Resource `yaml:"-"`
+
+	BackendName string `json:"backendName,omitempty" yaml:"backend_name,omitempty"`
+
+	Environment string `json:"environment,omitempty" yaml:"environment,omitempty"`
+
+	Hostname string `json:"hostname,omitempty" yaml:"hostname,omitempty"`
+
+	Path string `json:"path,omitempty" yaml:"path,omitempty"`
+
+	Priority int64 `json:"priority,omitempty" yaml:"priority,omitempty"`
+
+	Protocol string `json:"protocol,omitempty" yaml:"protocol,omitempty"`
+
+	Region string `json:"region,omitempty" yaml:"region,omitempty"`
+
+	Selector string `json:"selector,omitempty" yaml:"selector,omitempty"`
+
+	ServiceId string `json:"serviceId,omitempty" yaml:"service_id,omitempty"`
+
+	SourcePort int64 `json:"sourcePort,omitempty" yaml:"source_port,omitempty"`
+
+	TargetPort int64 `json:"targetPort,omitempty" yaml:"target_port,omitempty"`
+
+	Weight int64 `json:"weight,omitempty" yaml:"weight,omitempty"`
+}
+
+type PortRuleCollection struct {
+	Collection
+	Data   []PortRule `json:"data,omitempty"`
+	client *PortRuleClient
+}
+
+type PortRuleClient struct {
+	rancherClient *RancherClient
+}
+
+type PortRuleOperations interface {
+	List(opts *ListOpts) (*PortRuleCollection, error)
+	Create(opts *PortRule) (*PortRule, error)
+	Update(existing *PortRule, updates interface{}) (*PortRule, error)
+	ById(id string) (*PortRule, error)
+	Delete(container *PortRule) error
+}
+
+func newPortRuleClient(rancherClient *RancherClient) *PortRuleClient {
+	return &PortRuleClient{
+		rancherClient: rancherClient,
+	}
+}
+
+func (c *PortRuleClient) Create(container *PortRule) (*PortRule, error) {
+	resp := &PortRule{}
+	err := c.rancherClient.doCreate(PORT_RULE_TYPE, container, resp)
+	return resp, err
+}
+
+func (c *PortRuleClient) Update(existing *PortRule, updates interface{}) (*PortRule, error) {
+	resp := &PortRule{}
+	err := c.rancherClient.doUpdate(PORT_RULE_TYPE, &existing.Resource, updates, resp)
+	return resp, err
+}
+
+func (c *PortRuleClient) List(opts *ListOpts) (*PortRuleCollection, error) {
+	resp := &PortRuleCollection{}
+	err := c.rancherClient.doList(PORT_RULE_TYPE, opts, resp)
+	resp.client = c
+	return resp, err
+}
+
+func (cc *PortRuleCollection) Next() (*PortRuleCollection, error) {
+	if cc != nil && cc.Pagination != nil && cc.Pagination.Next != "" {
+		resp := &PortRuleCollection{}
+		err := cc.client.rancherClient.doNext(cc.Pagination.Next, resp)
+		resp.client = cc.client
+		return resp, err
+	}
+	return nil, nil
+}
+
+func (c *PortRuleClient) ById(id string) (*PortRule, error) {
+	resp := &PortRule{}
+	err := c.rancherClient.doById(PORT_RULE_TYPE, id, resp)
+	if apiError, ok := err.(*ApiError); ok {
+		if apiError.StatusCode == 404 {
+			return nil, nil
+		}
+	}
+	return resp, err
+}
+
+func (c *PortRuleClient) Delete(container *PortRule) error {
+	return c.rancherClient.doResourceDelete(PORT_RULE_TYPE, &container.Resource)
+}
