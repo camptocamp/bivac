90ed18a57517d37c06c864a464f7ae299d4acd7f
Jam Risser <jam@codejam.ninja>
2019-06-02 01:26:56 -0500

    Fixed parallel count env name

--- cmd/manager/manager.go
+++ cmd/manager/manager.go
@@ -114,7 +114,7 @@ func init() {
 	envs["BIVAC_WHITELIST_ANNOTATION"] = "whitelist.annotations"
 
 	managerCmd.Flags().IntVarP(&parallelCount, "parallel.count", "", 2, "The count of agents to run in parallel")
-	envs["VOLBACK_PARALLEL_COUNT"] = "parallel.count"
+	envs["BIVAC_PARALLEL_COUNT"] = "parallel.count"
 
 	bivacCmd.SetValuesFromEnv(envs, managerCmd.Flags())
 	bivacCmd.RootCmd.AddCommand(managerCmd)
