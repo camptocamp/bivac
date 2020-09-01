00d77ab480a443bf47b5199f696d0b57931a09dd
Léo Depriester <leo.depriester@camptocamp.com>
2020-09-01 13:46:15 +0200

    agent: avoid loop in log receiver

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -29,33 +29,31 @@ func Backup(targetURL, backupPath, hostname string, force bool, logReceiver stri
 	output := e.Backup(backupPath, hostname, force)
 
 	if logReceiver != "" {
-		for {
-			data := `{"data":` + output + `}`
-			req, err := http.NewRequest("POST", logReceiver, bytes.NewBuffer([]byte(data)))
-			if err != nil {
-				log.Errorf("failed to build new request: %s\n", err)
-				return
-			}
-			req.Header.Set("Content-Type", "application/json")
-			req.Header.Set("Authorization", "Bearer "+os.Getenv("BIVAC_SERVER_PSK"))
+		data := `{"data":` + output + `}`
+		req, err := http.NewRequest("POST", logReceiver, bytes.NewBuffer([]byte(data)))
+		if err != nil {
+			log.Errorf("failed to build new request: %s\n", err)
+			return
+		}
+		req.Header.Set("Content-Type", "application/json")
+		req.Header.Set("Authorization", "Bearer "+os.Getenv("BIVAC_SERVER_PSK"))
 
-			client := &http.Client{}
-			resp, err := client.Do(req)
-			if err != nil {
-				log.Errorf("failed to send data: %s\n", err)
-				continue
-			}
-			defer resp.Body.Close()
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			log.Errorf("failed to send data: %s\n", err)
+			return
+		}
+		defer resp.Body.Close()
 
-			b, err := ioutil.ReadAll(resp.Body)
-			if err != nil {
-				log.Errorf("failed to read body: %s\n", err)
-				return
-			}
-			if resp.StatusCode != 200 {
-				log.Infof("Response from API: %s", b)
-				return
-			}
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Errorf("failed to read body: %s\n", err)
+			return
+		}
+		if resp.StatusCode != 200 {
+			log.Infof("Response from API: %s", b)
+			return
 		}
 		return
 	}
