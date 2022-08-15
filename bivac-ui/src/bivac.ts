f9125e79e93fce48be5ebd49dc596f921c8c3dce
acul <acul009@gmail.com>
2022-08-15 04:56:40 +0200

    Added post to bivac api

--- bivac-ui/src/bivac.ts
+++ bivac-ui/src/bivac.ts
@@ -1,19 +1,28 @@
 var apiKey: string
 
 export default {
-    async setApiKey(key: string) {
+    setApiKey(key: string) {
         apiKey = key
     },
 
-    api(path: string): Promise<Response> {
-        return fetch(path, {
+    api(path: string, method: string = 'GET', body: string | object = undefined): Promise<Response> {
+        let options = {
+            method: method.toUpperCase(),
             headers: {
                 Authorization: "Bearer " + apiKey
             }
-        })
+        }
+        if (typeof body !== 'undefined') {
+            options.body = body
+        }
+        return fetch(path, options)
     },
 
     async get(path: string) {
+        return await this.api(path).then(res => res.json())
+    },
+
+    async post(path: string, body: string | object = null) {
         return await this.api(path).then(res => res.json())
     },
 
@@ -21,8 +30,12 @@ export default {
         const container = {
             timer: null,
             async reload() {
-                await handler()
-                this.timer = setTimeout(() => {this.reload();}, 5000)
+                try {
+                    await handler()
+                } catch (e) {
+                    console.log(e)
+                }
+                this.timer = setTimeout(() => { this.reload(); }, 5000)
             },
             cancel() {
                 clearTimeout(this.timer)
