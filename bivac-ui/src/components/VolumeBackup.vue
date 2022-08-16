c1810664c0589fcfc35550446d130f5eafbb83e7
acul <acul009@gmail.com>
2022-08-16 02:51:07 +0200

    Added additional components for volume control

---
+++ bivac-ui/src/components/VolumeBackup.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import bivac from '../bivac'
+import Table from './Table.vue'
+import Row from './Row.vue'
+import Cell from './Cell.vue'
+import { computed, onMounted, onUnmounted, reactive, ref } from 'vue';
+
+const props = defineProps<{
+  id: string
+}>()
+
+const vol = computed(() => {return bivac.volumes.value[props.id]})
+
+
+</script>
+
+<template>
+
+<button>Backup</button>
+
+</template>
+
+<style scoped>
+
+</style>
