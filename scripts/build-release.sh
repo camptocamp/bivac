1998cb39d895f7fcc1f599fa4e9acf69863a5659
Lucas Briese <lbriese@mail.upb.de>
2022-10-31 14:21:57 +0100

    style: shellcheck build-release

--- scripts/build-release.sh
+++ scripts/build-release.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-if [ -z $GO_VERSION ]; then
+if [ -z "$GO_VERSION" ]; then
   GO_VERSION=latest
 fi
 
-docker pull golang:$GO_VERSION
+docker pull "golang:$GO_VERSION"
 
 VERSION=$(git describe --always --dirty)
 
@@ -13,14 +13,14 @@ ARCHITECTURES=(386 amd64)
 
 mkdir -p release
 
-for platform in ${PLATFORMS[@]}; do
-  for arch in ${ARCHITECTURES[@]}; do
+for platform in "${PLATFORMS[@]}"; do
+  for arch in "${ARCHITECTURES[@]}"; do
     rm -f bivac
-    docker run -it --rm -w /go/src/github.com/camptocamp/bivac -v $(pwd):/go/src/github.com/camptocamp/bivac \
-      -e GOOS=${platform} \
-      -e GOARCH=${arch} \
-      golang:$GO_VERSION make bivac
+    docker run -it --rm -w "/go/src/github.com/camptocamp/bivac" -v "$(pwd):/go/src/github.com/camptocamp/bivac" \
+      -e "GOOS=${platform}" \
+      -e "GOARCH=${arch}" \
+      "golang:$GO_VERSION" make bivac
     sha256sum bivac >> release/SHA256SUM.txt
-    zip release/bivac_${VERSION}_${platform}_${arch}.zip bivac
+    zip "release/bivac_${VERSION}_${platform}_${arch}.zip" bivac
   done
 done
